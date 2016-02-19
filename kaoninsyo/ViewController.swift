730fd31e84117d7dbf642c46c47d618337e8ede0
sstakahashi <d.takahashi@strict.co.jp>
2016-02-19 18:10:34 +0900

    Initial Commit

---
+++ kaoninsyo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kaoninsyo
+//
+//  Created by takahashi daichi on 2016/02/19.
+//  Copyright © 2016年 strictsystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
